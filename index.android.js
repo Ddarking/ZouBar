bb9cd3dff0a4ab1d8274a222850039fd4d104e17
Mr.Mitol <mitol.huang@gmail.com>
2016-07-08 23:36:38 +0800

    初始化项目

---
+++ index.android.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import { //导包
+	AppRegistry,
+} from 'react-native';
+import Main from './app/pages/main'
+
+AppRegistry.registerComponent('React_test', () => Main);
